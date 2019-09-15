65b05a422404e133fbcaa2b0851e7832452e7968
AgustinArielGalarza <aguusg015@gmail.com>
2019-09-15 19:12:41 -0300

    primeros pasos Python

---
+++ 01.entrada_salida.py
@@ -0,0 +1,18 @@
+'''
+Ingrese un numero y solicitolo por pantalla
+
+'''
+def ingresaNum ():
+    print("Ingrese un numero: ")
+    entrada= input()
+    imprimeNum(entrada)
+
+def imprimeNum(mensaje):
+    print("Su numero es: ", mensaje)
+    
+def main():
+    ingresaNum()
+
+main()
+    
+    
